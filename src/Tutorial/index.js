c6ebc7abec42908bdbc1d2ec9bee956d47ebd37e
Tobias Tykvart <tobias.tykvart@posteo.de>
2022-02-12 14:50:28 +0100

    Fix typo

--- src/Tutorial/index.js
+++ src/Tutorial/index.js
@@ -8,5 +8,5 @@ export { styles, global, animation, icons };
 export * from './Avatar';
 export * from './Badge';
 export * from './Button';
-export * from './/Icon';
+export * from './Icon';
 export * from './TutorialLink';
