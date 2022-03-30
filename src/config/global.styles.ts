a01131a36dd2fb2348845f29e6760854a10e4b51
withchristopher <chmaxwel@icloud.com>
2022-03-30 23:44:02 +0200

    update to release workflow

---
+++ src/config/global.styles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    @font-face {
+
+    }
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+
+        font-family: 
+    }`
