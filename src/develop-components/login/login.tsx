5bf5e6d3bd99cc179a86b54cd1de14a60a5528af
Tobias Tykvart <tobias@factiverse.no>
2022-05-15 14:31:43 +0200

    Add test coverage workflow [skip ci] (#22) (#24)
    * Add test coverage workflow [skip ci]
    
    * Create coverage report on running tests
    
    * Add command to run tests in watch mode
    
    * Remove package-lock.json from gitignore
    
    * Run eslint only on src files
    
    * Add eslint to dependencies
    
    * Fix eslint config
    
    * Add proper eslint config and dependencies
    
    Fix eslint issues in files
    
    * Add missing dependency, test also on Node 17
    
    * Add missing dependency
    
    * Add missing dependency
    
    * Remove eslint settings for jsdoc
    
    * Add missing dependency
    
    * Add missing dependency
    
    * Add missing dependency

--- src/develop-components/login/login.tsx
+++ src/develop-components/login/login.tsx
@@ -8,78 +8,79 @@ import { H3 } from '../typography';
 import Link from '../link';
 
 export interface LoginModel {
-    email?: string;
-    password?: string;
+  email?: string;
+  password?: string;
 }
 
 export type SubmitHandler = (fieldValues: LoginModel) => void;
 
-export interface LoginProps extends Omit<HTMLAttributes<HTMLFormElement>, 'onSubmit'> {
-    onSubmit?: SubmitHandler;
-    initialValues?: LoginModel;
-    loading?: boolean;
-    registerLink?: string;
+export interface LoginProps
+  extends Omit<HTMLAttributes<HTMLFormElement>, 'onSubmit'> {
+  onSubmit?: SubmitHandler;
+  initialValues?: LoginModel;
+  loading?: boolean;
+  registerLink?: string;
 }
 
-const Login: React.ForwardRefRenderFunction<HTMLFormElement, LoginProps> = (props) => {
-    const {
-        onSubmit = () => {},
-        initialValues = {},
-        loading = false,
-        registerLink = '#',
-    } = props;
+const Login: React.ForwardRefRenderFunction<HTMLFormElement, LoginProps> = (
+  props
+) => {
+  const {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onSubmit = () => {},
+    initialValues = {},
+    loading = false,
+    registerLink = '#',
+  } = props;
 
-    const {
-        values,
-        errors,
-        handleChange,
-        handleSubmit,
-      } = useLoginLogic(
-        onSubmit,
-        initialValues,
-      );
-    return (
-        <StyledForm>
-            <H3 align='center'>Sign In</H3>
-            <FormControl
-                label='Email'
-                htmlFor='email'
-                error={errors.email}
-                forceLabel
-            >
-                <TextInput
-                    id='email'
-                    value={values.email}
-                    placeholder='Enter email'
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange('email')(e.currentTarget.value)}
-                    error={!!errors.email}
-                />
-            </FormControl>
-            <FormControl
-                label='Password'
-                htmlFor='password'
-                error={errors.password}
-                forceLabel
-            >
-                <TextInput
-                    id='password'
-                    type='password'
-                    value={values.password}
-                    placeholder='Enter password'
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange('password')(e.currentTarget.value)}
-                    error={!!errors.password}
-                />
-            </FormControl>
-            <ButtonWrapper>
-                <Link href={registerLink}>Create account</Link>
-                <ButtonText
-                    onClick={handleSubmit}
-                    loading={loading}
-                >Proceed</ButtonText>
-            </ButtonWrapper>
-        </StyledForm>
-    );
-}
-
+  const { values, errors, handleChange, handleSubmit } = useLoginLogic(
+    onSubmit,
+    initialValues
+  );
+  return (
+    <StyledForm>
+      <H3 align="center">Sign In</H3>
+      <FormControl
+        label="Email"
+        htmlFor="email"
+        error={errors.email}
+        forceLabel
+      >
+        <TextInput
+          id="email"
+          value={values.email}
+          placeholder="Enter email"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            handleChange('email')(e.currentTarget.value)
+          }
+          error={!!errors.email}
+        />
+      </FormControl>
+      <FormControl
+        label="Password"
+        htmlFor="password"
+        error={errors.password}
+        forceLabel
+      >
+        <TextInput
+          id="password"
+          type="password"
+          value={values.password}
+          placeholder="Enter password"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            handleChange('password')(e.currentTarget.value)
+          }
+          error={!!errors.password}
+        />
+      </FormControl>
+      <ButtonWrapper>
+        <Link href={registerLink}>Create account</Link>
+        <ButtonText onClick={handleSubmit} loading={loading}>
+          Proceed
+        </ButtonText>
+      </ButtonWrapper>
+    </StyledForm>
+  );
+};
 
 export default React.forwardRef<HTMLFormElement, LoginProps>(Login);
--- src/develop-components/skeleton/skeleton.stories.tsx
+++ src/develop-components/skeleton/skeleton.stories.tsx
@@ -13,13 +13,13 @@ const Template: Story<SkeletonProps> = (args) => <Skeleton {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    width: 150,
-    height: 150,
-}
+  width: 150,
+  height: 150,
+};
 
 export const BorderRadius = Template.bind({});
 BorderRadius.args = {
-    width: 150,
-    height: 150,
-    borderRadius: '50%',
-}
+  width: 150,
+  height: 150,
+  borderRadius: '50%',
+};
