e4cf7e17330bb595985a4c9acdaf5673f51ccede
Tobias Tykvart <tobias.tykvart@posteo.de>
2022-06-07 17:59:18 +0200

    Add test for showing/hiding password

--- src/components/PasswordField/PasswordField.test.tsx
+++ src/components/PasswordField/PasswordField.test.tsx
@@ -9,15 +9,55 @@ describe('PasswordField component', () => {
     render(<PasswordField actions={defaultPasswordActions} />);
   });
 
-  it('should render Password eye icon hide and show', () => {
+  it('should make the password readable.', async () => {
     expect(screen.getByLabelText('visibility')).toBeInTheDocument();
     expect(screen.queryByLabelText('visibilityOff')).not.toBeInTheDocument();
+    expect(screen.getByDisplayValue('passwordValue')).not.toHaveAttribute(
+      'type',
+      'text'
+    );
+    expect(screen.getByDisplayValue('passwordValue')).toHaveAttribute(
+      'type',
+      'password'
+    );
+    await userEvent.click(screen.getByLabelText('visibility'));
+    expect(screen.queryByLabelText('visibility')).not.toBeInTheDocument();
+    expect(screen.getByLabelText('visibilityOff')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('passwordValue')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('passwordValue')).toHaveAttribute(
+      'type',
+      'text'
+    );
+    expect(screen.getByDisplayValue('passwordValue')).not.toHaveAttribute(
+      'type',
+      'password'
+    );
+    await userEvent.click(screen.getByLabelText('visibilityOff'));
+    expect(screen.getByLabelText('visibility')).toBeInTheDocument();
+    expect(screen.queryByLabelText('visibilityOff')).not.toBeInTheDocument();
+    expect(screen.getByDisplayValue('passwordValue')).not.toHaveAttribute(
+      'type',
+      'text'
+    );
+    expect(screen.getByDisplayValue('passwordValue')).toHaveAttribute(
+      'type',
+      'password'
+    );
   });
 
-  it('should render Password caps lock warning', async () => {
+  it('should show caps lock warning.', async () => {
+    // caps lock warning should not be visible by default
     expect(screen.queryByLabelText('warning')).not.toBeInTheDocument();
+    // click the input field to put the focus on it
     await userEvent.click(screen.getByLabelText('Password'));
     await userEvent.keyboard('[CapsLock]');
     expect(screen.getByLabelText('warning')).toBeInTheDocument();
+  });
+
+  it('should hide caps lock warning.', async () => {
+    expect(screen.queryByLabelText('warning')).not.toBeInTheDocument();
+    // press caps lock twice to activate and deactivate it
+    await userEvent.keyboard('[CapsLock][CapsLock]');
+    expect(screen.queryByLabelText('warning')).not.toBeInTheDocument();
   });
 });
