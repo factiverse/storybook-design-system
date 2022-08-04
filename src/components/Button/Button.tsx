63223ee8cbb28a7c30a43fbd3ba45a1787f19aa8
Tobias Tykvart <tobias.tykvart@posteo.de>
2022-08-04 11:05:39 +0200

    Add Typography documentation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Button as ButtonMUI, ButtonProps } from '@mui/material';
 
 export const Button = ({ ...props }: ButtonProps) => {
-  return <ButtonMUI disableRipple sx={{ borderRadius: '8px' }} {...props} />;
-};
-
-Button.defaultProps = {
-  variant: 'contained',
+  const { variant = 'contained' } = props;
+  return (
+    <ButtonMUI
+      disableRipple
+      sx={{ borderRadius: '8px' }}
+      variant={variant}
+      {...props}
+    />
+  );
 };
 
 export default Button;
