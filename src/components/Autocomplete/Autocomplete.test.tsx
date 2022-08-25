604535e82d1de6d8cca9cfb3335cabc5540a86a7
Tobias Tykvart <tobias.tykvart@posteo.de>
2022-08-25 10:13:25 +0200

    Fix Autocomplete test

--- src/components/Autocomplete/Autocomplete.test.tsx
+++ src/components/Autocomplete/Autocomplete.test.tsx
@@ -7,19 +7,17 @@ import Autocomplete from './Autocomplete';
 describe('Autocomplete', () => {
   it('should render autocomplete component', () => {
     const data = [];
-    render(<Autocomplete data={data} id={'1'} />);
-    expect(screen.getByLabelText(/Search input/)).toBeInTheDocument();
+    render(<Autocomplete data={data} label="Search Input" />);
+    expect(screen.getByLabelText('Search Input')).toBeInTheDocument();
   });
 
   it('should suggest sentences.', () => {
     const data = [
-      {
-        title: 'Russia has no plans to occupy Ukraine.',
-        year: 2022,
-      },
+      'Russia has no plans to occupy Ukraine.',
+      'There are U.S. biolabs in Ukraine funded by the U.S. government.',
     ];
-    render(<Autocomplete data={data} id={'1'} />);
-    userEvent.click(screen.getByLabelText(/Search input/));
+    render(<Autocomplete data={data} label="Search Input" />);
+    userEvent.click(screen.getByLabelText('Search Input'));
 
     expect(
       screen.getByText('Russia has no plans to occupy Ukraine.')
