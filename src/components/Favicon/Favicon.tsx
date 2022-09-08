21a0c211f9d31bb4eed877cf655841f0e94cfdf3
Tobias Tykvart <tobias.tykvart@posteo.de>
2022-09-08 14:12:16 +0200

    Add documentation for favicon

--- src/components/Favicon/Favicon.tsx
+++ src/components/Favicon/Favicon.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable new-cap */
-import { Grid, Paper } from '@mui/material';
-import React from 'react';
+import { Paper } from '@mui/material';
+import React, { useState } from 'react';
 interface FaviconProps {
   /* The link displayed in the UI */
   domain: string;
@@ -8,29 +8,25 @@ interface FaviconProps {
 
 /* A component to display the link to the article and its publishing date */
 const Favicon: React.FC<FaviconProps> = ({ domain }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  const onLoad = () => {
+    console.log('loaded');
+    setLoaded(true);
+  };
+
   return (
-    <Paper sx={{ height: 34, width: 34 }} variant="outlined" elevation={3}>
-      <Grid>
-        <Grid
-          item
-          sx={{ height: 34, width: 34 }}
-          display="flex"
-          container
-          alignItems="center"
-          justifyContent="center"
-        >
-          <img
-            height={32}
-            width={32}
-            src={'https://www.' + domain + '/favicon.ico'}
-            alt={'favicon ' + domain}
-            // If no favicon was found then hide the element
-            onError={(i) =>
-              ((i.target as HTMLImageElement).style.display = 'none')
-            }
-          />
-        </Grid>
-      </Grid>
+    <Paper
+      sx={{ height: 34, width: 34, display: loaded ? 'block' : 'none' }}
+      variant="outlined"
+      elevation={3}
+    >
+      <img
+        width={32}
+        src={'https://www.' + domain + '/favicon.ico'}
+        alt={'favicon ' + domain}
+        onLoad={onLoad}
+      />
     </Paper>
   );
 };
