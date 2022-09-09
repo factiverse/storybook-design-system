33a962ca987e648dc59324b4e20e7d8cfe4aa42d
withchristopher <chmaxwel@icloud.com>
2022-09-09 18:12:13 +0200

    updated package.json to build without cjs

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,9 @@ export default {
       browser: true,
     }),
     commonjs(),
-    typescript(),
+    typescript({
+      tsconfig: './tsconfig.build.json',
+    }),
     postcss(),
     image(),
     visualizer({
