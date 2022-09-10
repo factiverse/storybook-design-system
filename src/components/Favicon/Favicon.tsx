ec0a0e90609f1030c73c2924562c989af15b3d9a
Tobias Tykvart <tobias@factiverse.no>
2022-09-10 17:26:29 +0200

    Find favicons in more cases (#61)
    * Add documentation for Favicon
    
    * Revert "Add documentation for Favicon"
    
    This reverts commit 8d53d9082fb1c7f2e92c794bb6265c864df1d9b4.
    
    * Add documentation for favicon
    
    * Use duckduckgo to get favicon

--- src/components/Favicon/Favicon.tsx
+++ src/components/Favicon/Favicon.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable new-cap */
-import { Grid, Paper } from '@mui/material';
+import { Paper } from '@mui/material';
 import React from 'react';
 interface FaviconProps {
   /* The link displayed in the UI */
@@ -10,27 +10,12 @@ interface FaviconProps {
 const Favicon: React.FC<FaviconProps> = ({ domain }) => {
   return (
     <Paper sx={{ height: 34, width: 34 }} variant="outlined" elevation={3}>
-      <Grid>
-        <Grid
-          item
-          sx={{ height: 34, width: 34 }}
-          display="flex"
-          container
-          alignItems="center"
-          justifyContent="center"
-        >
-          <img
-            height={32}
-            width={32}
-            src={'https://www.' + domain + '/favicon.ico'}
-            alt={'favicon ' + domain}
-            // If no favicon was found then hide the element
-            onError={(i) =>
-              ((i.target as HTMLImageElement).style.display = 'none')
-            }
-          />
-        </Grid>
-      </Grid>
+      <img
+        width={32}
+        // DuckDuckGo scrapes favicons and catches all different ways of adding them to a website
+        src={'https://icons.duckduckgo.com/ip2/www.' + domain + '.ico'}
+        alt={'favicon ' + domain}
+      />
     </Paper>
   );
 };
