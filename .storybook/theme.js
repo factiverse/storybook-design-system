64b0da3041c2555b51fc93ba489b1a4aefd07bc8
Domante Stirbyte <domante.stirbyte@gmail.com>
2022-09-22 12:48:40 +0200

    fix: added logo and img file

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import Logo from './Logo.png';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Factiverse Design System',
+  brandUrl: 'http://factiverse.no/',
+  brandImage: Logo,
+});
