e50f6485263689e09610d2fa9534c6a46ce6bebc
Tobias Tykvart <tobias.tykvart@posteo.de>
2023-01-17 10:20:52 +0100

    Clean given URLs

--- src/components/Favicon/Favicon.tsx
+++ src/components/Favicon/Favicon.tsx
@@ -3,11 +3,11 @@ import { Paper } from '@mui/material';
 import React from 'react';
 export interface FaviconProps {
   /**
-   * The favicon is fetched for this given domain.
+   * Ideally only root domains should be given but most URLs will work.
    *
    * @example google.com
    */
-  domain: string;
+  rootDomain: string;
 }
 
 /**
@@ -15,14 +15,48 @@ export interface FaviconProps {
  *
  * @return {React.FC}
  */
-const Favicon: React.FC<FaviconProps> = ({ domain }) => {
+const Favicon: React.FC<FaviconProps> = ({ rootDomain }) => {
+  /**
+   * Removes the protocol, subdomain and folders (if any) from a given URL to get only the root domain.
+   *
+   * @example
+   * https://topics.factiverse.no/home gets turned into factiverse.no
+   *
+   * @param {string} url
+   * @return {string} The root domain
+   */
+  const getRootDomain = (url: string) => {
+    // TODO: This function fails if there are multiple domain names but no subdomain, is there a better way than including common exceptions?
+    // Special cases
+    if (url.includes('bbc.co.uk')) return 'bbc.co.uk';
+
+    // Remove folders and protocol
+    const splitUrl = url.split('/');
+    let domain = '';
+    for (let i = 0; i < splitUrl.length; i++) {
+      if (splitUrl[i].includes('.')) {
+        domain = splitUrl[i];
+        break;
+      }
+    }
+    // Remove subdomain
+    const splitDomain = domain.split('.');
+    const length = splitDomain.length;
+    // If there is no subdomain it simply returns, otherwise it removes the subdomain
+    return length == 2 ? splitDomain.join('.') : splitDomain.slice(1).join('.');
+  };
+
   return (
     <Paper sx={{ height: 34, width: 34 }} variant="outlined" elevation={3}>
       <img
         width={32}
         // DuckDuckGo scrapes favicons and catches all different ways of adding them to a website
-        src={'https://icons.duckduckgo.com/ip2/www.' + domain + '.ico'}
-        alt={'favicon ' + domain}
+        src={
+          'https://icons.duckduckgo.com/ip2/www.' +
+          getRootDomain(rootDomain) +
+          '.ico'
+        }
+        alt={'favicon ' + getRootDomain(rootDomain)}
       />
     </Paper>
   );
