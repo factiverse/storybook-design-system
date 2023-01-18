a3f87277774c83a29a284a0818ab04ec6d26536b
Tobias Tykvart <tobias@factiverse.no>
2023-01-18 16:08:42 +0100

    Update color doc

--- util/templates/component.js
+++ util/templates/component.js
@@ -4,6 +4,9 @@ import React from 'react';
 import { ${componentName}Props } from './${componentName}.types';
 import './${componentName}.scss';
 
+/**
+ * Describe what this component is used for.
+ */
 const ${componentName}: React.FC<${componentName}Props> = ({ foo }) => (
   <div data-testid="TestComponent" className="foo-bar">
     {foo}
